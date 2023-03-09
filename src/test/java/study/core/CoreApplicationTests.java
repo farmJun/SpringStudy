57071cf7db9fbbed437d20b46d18ecb774ec3a0b
박준영 <wnsdud6969@naver.com>
2023-03-09 23:04:26 +0900

    프로젝트 생성

---
+++ src/test/java/study/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package study.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
