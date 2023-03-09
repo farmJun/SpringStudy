cfadb6633bfdf6c7cbff0d09dff51b2d9b3b1fe1
박준영 <wnsdud6969@naver.com>
2023-03-09 23:28:19 +0900

    [Feat]: MemoryServiceImpl 생성

---
+++ src/main/java/study/core/member/MemoryServiceImpl.java
@@ -0,0 +1,16 @@
+package study.core.member;
+
+public class MemoryServiceImpl implements MemoryService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
