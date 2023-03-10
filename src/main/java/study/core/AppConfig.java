bd4b0d94bd86365de75e77845d3152f3b6195ade
박준영 <wnsdud6969@naver.com>
2023-03-10 16:23:40 +0900

    [Refactor]: AppConfig 리팩토링

--- src/main/java/study/core/AppConfig.java
+++ src/main/java/study/core/AppConfig.java
@@ -1,6 +1,8 @@
 package study.core;
 
+import study.core.discount.DiscountPolicy;
 import study.core.discount.FixDiscountPolicy;
+import study.core.member.MemberRepository;
 import study.core.member.MemberService;
 import study.core.member.MemberServiceImpl;
 import study.core.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import study.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
