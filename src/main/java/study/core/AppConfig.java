afb25de42d785c3406f34811119793bcf09adc01
박준영 <wnsdud6969@naver.com>
2023-03-12 18:14:40 +0900

    [Refactor]: Spring Bean 등록

--- src/main/java/study/core/AppConfig.java
+++ src/main/java/study/core/AppConfig.java
@@ -1,5 +1,7 @@
 package study.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import study.core.discount.DiscountPolicy;
 import study.core.discount.FixDiscountPolicy;
 import study.core.member.MemberRepository;
@@ -9,20 +11,25 @@ import study.core.member.MemoryMemberRepository;
 import study.core.order.OrderService;
 import study.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemberRepository memberRepository() {
+    @Bean
+    public static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
