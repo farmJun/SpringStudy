6dce41c65718e6df2f98d6c57fedd555e82924b1
박준영 <wnsdud6969@naver.com>
2023-03-22 23:38:19 +0900

    [Feat]: Configuration 테스트를 위한 getMemberRepository() 추가

--- src/main/java/study/core/member/MemberServiceImpl.java
+++ src/main/java/study/core/member/MemberServiceImpl.java
@@ -16,4 +16,8 @@ public class MemberServiceImpl implements MemberService {
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
+    }
 }
