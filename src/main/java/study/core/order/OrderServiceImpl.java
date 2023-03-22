5fb5823ede8bbc931476c152f700099da6fb3346
박준영 <wnsdud6969@naver.com>
2023-03-22 23:39:09 +0900

    [Feat]: Configuration 테스트를 위한 getMemberRepository() 추가

--- src/main/java/study/core/order/OrderServiceImpl.java
+++ src/main/java/study/core/order/OrderServiceImpl.java
@@ -6,7 +6,7 @@ import study.core.member.MemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository ;
+    private final MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
@@ -20,5 +20,9 @@ public class OrderServiceImpl implements OrderService {
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
