7334cdc5feb0f8a2faf6f76ba058a906176f597f
박준영 <wnsdud6969@naver.com>
2023-03-22 23:40:46 +0900

    [Fix]: memberServiceImpl, orderServiceImpl의 인스턴스가 다른 문제 해결

--- src/main/java/study/core/AppConfig.java
+++ src/main/java/study/core/AppConfig.java
@@ -20,7 +20,7 @@ public class AppConfig {
     }
 
     @Bean
-    public static MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
