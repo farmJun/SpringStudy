5241a78adee295ffae9665e32798cabe458d8c05
박준영 <wnsdud6969@naver.com>
2023-03-24 23:44:20 +0900

    [Feat]: @Component, @Autowired 어노테이션 추가

--- src/main/java/study/core/order/OrderServiceImpl.java
+++ src/main/java/study/core/order/OrderServiceImpl.java
@@ -1,14 +1,18 @@
 package study.core.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import study.core.discount.DiscountPolicy;
 import study.core.member.Member;
 import study.core.member.MemberRepository;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
