e2a2d10804155729934a54d7002566bd02c6a62c
박준영 <wnsdud6969@naver.com>
2023-03-24 23:42:29 +0900

    [Feat]: @Component 어노테이션 추가

--- src/main/java/study/core/discount/RateDiscountPolicy.java
+++ src/main/java/study/core/discount/RateDiscountPolicy.java
@@ -1,8 +1,10 @@
 package study.core.discount;
 
+import org.springframework.stereotype.Component;
 import study.core.member.Grade;
 import study.core.member.Member;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10;
 
