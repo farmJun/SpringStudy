db2bfd3426df58fa4065edb76f2b10fbac69264d
박준영 <wnsdud6969@naver.com>
2023-03-26 23:47:11 +0900

    [Test]: AutoAppConfig 테스트

---
+++ src/test/java/study/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,18 @@
+package study.core.scan;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import study.core.AutoAppConfig;
+import study.core.member.MemberService;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
