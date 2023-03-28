3e66584632dc3da5783e131ed11bb9af59e95c49
박준영 <wnsdud6969@naver.com>
2023-03-29 00:16:48 +0900

    [Test]: 컴포넌트 스캔 대상에 제외할 애노테이션

---
+++ src/test/java/study/core/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,9 @@
+package study.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
