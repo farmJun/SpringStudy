9631302bb86c7a8342505eb650f339706812a895
박준영 <wnsdud6969@naver.com>
2023-03-29 00:16:38 +0900

    [Test]: 컴포넌트 스캔 대상에 추가할 애노테이션

---
+++ src/test/java/study/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,10 @@
+package study.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+
+}
