1cb7d5fdbd7cb301c6469f2bd4c29f599e81c110
박준영 <wnsdud6969@naver.com>
2023-03-31 22:52:03 +0900

    [Test]: @Autowired(required=false)를 통해 자동 주입 대상을 옵션으로 처리

---
+++ src/test/java/study/core/autowired/AutowiredTest.java
@@ -0,0 +1,27 @@
+package study.core.autowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+import study.core.member.Member;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void autoWiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+    }
+}
