126d002f3969c13f24810f1ae50e4aa8fd90c4a2
박준영 <wnsdud6969@naver.com>
2023-04-02 18:30:49 +0900

    [Feat]:@Qualifier를 통해 조회 빈이 2개 이상일 때 문제 해결

--- src/main/java/study/core/order/OrderServiceImpl.java
+++ src/main/java/study/core/order/OrderServiceImpl.java
@@ -2,17 +2,22 @@ package study.core.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import study.core.discount.DiscountPolicy;
 import study.core.member.Member;
 import study.core.member.MemberRepository;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
