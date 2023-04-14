494d5e446e610fd8e46f3fc2d6491447fd7aece6
박준영 <wnsdud6969@naver.com>
2023-04-14 23:41:20 +0900

    [Fix]:Provider를 사용해 request scope 빈 생성을 지연을 통한 오류 해결

--- src/main/java/study/core/common/LogDemoController.java
+++ src/main/java/study/core/common/LogDemoController.java
@@ -2,6 +2,7 @@ package study.core.common;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,14 +12,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        myLogger.setRequestURL(requestURL);
+        MyLogger myLogger = myLoggerProvider.getObject();
 
+        myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");
         return "OK";
--- src/main/java/study/core/common/LogDemoService.java
+++ src/main/java/study/core/common/LogDemoService.java
@@ -1,13 +1,16 @@
 package study.core.common;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
