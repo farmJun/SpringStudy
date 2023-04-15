a2ded7cba2383ed1420b34b5ea8ae78e28a441cb
박준영 <wnsdud6969@naver.com>
2023-04-15 23:14:23 +0900

    [Refactor]:프록시 방식을 사용해 request scope 빈 생성을 지연

--- src/main/java/study/core/common/LogDemoController.java
+++ src/main/java/study/core/common/LogDemoController.java
@@ -2,7 +2,6 @@ package study.core.common;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,16 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
-
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
-
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");
