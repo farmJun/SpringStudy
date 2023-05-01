3b4fd7258fa84812fe8918e865fd6996c5c74955
박준영 <wnsdud6969@naver.com>
2023-05-01 23:12:39 +0900

    [Feat]: 의도적으로 수동 빈 등록, 자동 빈 등록 충돌 확인

--- src/main/java/study/core/AutoAppConfig.java
+++ src/main/java/study/core/AutoAppConfig.java
@@ -1,15 +1,21 @@
 package study.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import study.core.member.MemberRepository;
+import study.core.member.MemoryMemberRepository;
 
 @Configuration
-@ComponentScan(
-        basePackages = "study.core",
-        excludeFilters =  @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-)
+@ComponentScan(basePackages = "study.core",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
+                classes = Configuration.class))
 public class AutoAppConfig {
 
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
